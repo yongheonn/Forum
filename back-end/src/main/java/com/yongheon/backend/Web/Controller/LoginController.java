e54e25f8ccaa0eef65a529615c78cacbf811a076
yongheonn <tiger9966@naver.com>
2023-04-11 04:02:39 +0900

    test(back-end): test to solve cookie issue

--- back-end/src/main/java/com/yongheon/backend/Web/Controller/LoginController.java
+++ back-end/src/main/java/com/yongheon/backend/Web/Controller/LoginController.java
@@ -76,7 +76,7 @@ public class LoginController {
 			Cookie cookie = new Cookie("refreshToken", refreshToken);
 			cookie.setHttpOnly(true);
 			cookie.setSecure(true);
-			cookie.setPath("/ajax/auth/refresh/");
+			cookie.setPath(api_url + "/ajax/auth/refresh/");
 			cookie.setDomain("yongheonn.com");
 			cookie.setMaxAge(8200000);
 			response.addCookie(cookie);
