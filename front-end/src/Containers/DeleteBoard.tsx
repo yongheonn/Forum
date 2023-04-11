2ae35fcefe29cf442e494fc097722c30f5c98bd2
yongheonn <tiger9966@naver.com>
2023-04-12 01:19:56 +0900

    test(front-end): test aws lambda

--- front-end/src/Containers/DeleteBoard.tsx
+++ front-end/src/Containers/DeleteBoard.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import ModalPopup from '../Components/ModalPopup';
