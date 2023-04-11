916d690aa6d10fe85bae9ddd312bd2d62cb7b421
yongheonn <tiger9966@naver.com>
2023-04-12 01:26:16 +0900

    test(front-end): test aws lambda

--- front-end/src/Containers/BottomPanel.tsx
+++ front-end/src/Containers/BottomPanel.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { AiOutlineSetting } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { HorizontalPanel, VerticalPanel } from '../Components/Panel';
 import AccountMenu from './AccountMenu';
