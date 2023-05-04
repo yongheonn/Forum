2c51f56c6dd15fe412da8484f72728582f237684
yongheonn <tiger9966@naver.com>
2023-05-04 20:21:42 +0900

    fix(back-end): fix register dto

--- back-end/src/main/java/com/yongheon/backend/Web/DTO/RegisterDTO.java
+++ back-end/src/main/java/com/yongheon/backend/Web/DTO/RegisterDTO.java
@@ -1,8 +1,12 @@
 package com.yongheon.backend.Web.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterDTO {
     private String id;
     private String pw;
