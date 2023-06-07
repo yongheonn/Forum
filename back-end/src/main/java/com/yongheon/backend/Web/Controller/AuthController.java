893b3f099fd46e5c12c47420fc4d9f0eb1ce0c22
yongheonn <tiger9966@naver.com>
2023-06-08 02:18:06 +0900

    test

--- back-end/src/main/java/com/yongheon/backend/Web/Controller/AuthController.java
+++ back-end/src/main/java/com/yongheon/backend/Web/Controller/AuthController.java
@@ -3,7 +3,6 @@ package com.yongheon.backend.Web.Controller;
 import java.io.IOException;
 
 import javax.inject.Inject;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
