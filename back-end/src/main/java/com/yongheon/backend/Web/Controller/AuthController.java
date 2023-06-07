b2be34ad87206cf8a16f1c4d51377ac557be123f
yongheonn <tiger9966@naver.com>
2023-06-08 02:10:38 +0900

    test

--- back-end/src/main/java/com/yongheon/backend/Web/Controller/AuthController.java
+++ back-end/src/main/java/com/yongheon/backend/Web/Controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
